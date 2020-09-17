bce80957107c4a8bc8d013a22055b245344992c9
40306822865 <71472048+40306822865@users.noreply.github.com>
2020-09-17 20:50:16 -0300

    Create application.py

---
+++ application.py
@@ -0,0 +1,9 @@
+from  flask  importar  Flask
+aplicação  =  Frasco ( __name__ )
+
+@ aplicativo . rota ( "/" )
+def  olá ():
+    return  "<h1> Hola FIAP! </h1> \ n MBA! o /"
+
+if  __name__  ==  '__main__' :
+    aplicação . run ()
